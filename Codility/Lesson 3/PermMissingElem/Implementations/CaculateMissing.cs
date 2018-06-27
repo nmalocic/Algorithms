e77d3f86bbcf8149805002de4f9d094537a8e5c4
Zlatko Radojcic <zlatko.rad@gmail.com>
2018-06-28 00:00:21 +0200

    Tests and solution for PermMissingElem problem

---
+++ Codility/Lesson 3/PermMissingElem/Implementations/CaculateMissing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Codility.Lesson_3.PermMissingElem
+{
+    class CaculateMissing : IPermMissingElem
+    {
+        public int solution(int[] A)
+        {
+            var n = A.Length + 1;
+
+            return CalculateSumUpToNumber(n) - A.Sum();
+        }
+
+        private int CalculateSumUpToNumber(int n)
+        {
+            return (n * (n + 1)) / 2;
+        }
+    }
+}
