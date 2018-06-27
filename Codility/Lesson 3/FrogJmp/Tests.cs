59ed1bee65353a172f2e366dd7ded9f4497a7b36
Zlatko Radojcic <zlatko.rad@gmail.com>
2018-06-27 23:40:07 +0200

    Added Tests for FrogJmp

---
+++ Codility/Lesson 3/FrogJmp/Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Codility.Lesson_3.FrogJmp
+{
+    public class Tests
+    {
+        /// <summary>
+        /// This should return solution that you want to test.
+        /// </summary>
+        /// <returns></returns>
+        private IFrogJmp GetImplementation()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Basic_example_1()
+        {
+            //Arrange
+            var X = 10;
+            var Y = 85;
+            var D = 30;
+
+            var expected = 3;
+
+            //Act
+            var result = GetImplementation().solution(X, Y, D);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        
+    }
+}
