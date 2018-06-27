ebcc3eb45a4cc1d059f1a9485535accfcf7719b9
Nemanja Malocic <n.malocic@emakina.nl>
2018-06-27 10:19:07 +0200

    Refactored unit test for lesson 1

--- Codility/Lesson 1/BinaryGapTest.cs
+++ Codility/Lesson 1/BinaryGapTest.cs
@@ -1,7 +1,7 @@
-using System;
 using Xunit;
 
-namespace Lesson1 {
+namespace Lesson1
+{
     public class BinaryGapTest {
 
         private BinaryGap _binaryGap {get; set;}
@@ -11,49 +11,18 @@ namespace Lesson1 {
             _binaryGap = new BinaryGap();
         }
 
-        [Fact]
-        public void Given_Number_9_Returns_2() 
+        [Theory]
+        [InlineData(9, 2)]
+        [InlineData(529, 4)]
+        [InlineData(20, 1)]
+        [InlineData(32, 0)]
+        [InlineData(2147483647, 0)]
+        [InlineData(1073741825, 29)]
+        public void TestSolution(int input, int longestGap)
         {
-            int result = _binaryGap.solution(9);
+            int result = _binaryGap.solution(input);
 
-            Assert.Equal(2, result);
-        }
-
-        [Fact]
-        public void Given_Number_529_Returns_4()
-        {
-            int result = _binaryGap.solution(529);
-
-            Assert.Equal(4, result);
-        }
-
-        [Fact]
-        public void Given_Number_20_Returns_1()
-        {
-            int result = _binaryGap.solution(20);
-
-            Assert.Equal(1, result);
-        }
-
-        
-        [Fact]
-        public void Given_Number_32_Returns_0()
-        {
-            int result = _binaryGap.solution(32);
-
-            Assert.Equal(0, result);
-        }
-
-        [Fact]
-        public void Given_Number_2147483647_Returns_0()
-        {
-            Assert.Equal(0, _binaryGap.solution(2147483647));
-        }
-
-        [Fact]
-        public void Given_Number_1073741825_Returns_29()
-        {
-            Assert.Equal(29, _binaryGap.solution(1073741825));
+            Assert.Equal(longestGap, result);
         }
     }
 }
