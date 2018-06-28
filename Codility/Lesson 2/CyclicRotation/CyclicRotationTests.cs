d378e0ff248daf96801ce84864f7aff5d93b5b56
Nemanja Malocic <n.malocic@emakina.nl>
2018-06-28 13:12:54 +0200

    Refactored tests and added new solutions

---
+++ Codility/Lesson 2/CyclicRotation/CyclicRotationTests.cs
@@ -0,0 +1,56 @@
+﻿using Xunit;
+using Codility.Lesson_2.CyclicRotation.Implementations;
+
+namespace Codility.Lesson_2.CyclicRotation
+{
+    /// <summary>
+    /// Used to test solutions for Cyclic rotation assignment.
+    /// </summary>
+    public class CyclicRotationTests
+    {
+        [Theory]
+        [InlineData(new[] {3, 8, 9, 7, 6}, 3, new[] { 9, 7, 6, 3, 8 })]
+        [InlineData(new[] { 0, 0, 0 }, 1, new[] { 0, 0, 0 })]
+        [InlineData(new[] { 1, 2, 3, 4 }, 4, new[] { 1, 2, 3, 4 })]
+        public void UsingEnumerableTests(int[] inputArray, int offset, int[] expected)
+        {
+            ICyclicRotation cyclicRotation = new UsingEnumerable();
+
+            //Act
+            var result = cyclicRotation.solution(inputArray, offset);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(new[] { 3, 8, 9, 7, 6 }, 3, new[] { 9, 7, 6, 3, 8 })]
+        [InlineData(new[] { 0, 0, 0 }, 1, new[] { 0, 0, 0 })]
+        [InlineData(new[] { 1, 2, 3, 4 }, 4, new[] { 1, 2, 3, 4 })]
+        public void UsingLinq(int[] inputArray, int offset, int[] expected)
+        {
+            ICyclicRotation cyclicRotation = new UsingLinq();
+
+            //Act
+            var result = cyclicRotation.solution(inputArray, offset);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(new[] { 3, 8, 9, 7, 6 }, 3, new[] { 9, 7, 6, 3, 8 })]
+        [InlineData(new[] { 0, 0, 0 }, 1, new[] { 0, 0, 0 })]
+        [InlineData(new[] { 1, 2, 3, 4 }, 4, new[] { 1, 2, 3, 4 })]
+        public void UsingCalclucaltion(int[] inputArray, int offset, int[] expected)
+        {
+            ICyclicRotation cyclicRotation = new CalculatingNextIndex();
+
+            //Act
+            var result = cyclicRotation.solution(inputArray, offset);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ Codility/Lesson 2/CyclicRotation/Implementations/CalculatingNextIndex.cs
@@ -0,0 +1,18 @@
+﻿namespace Codility.Lesson_2.CyclicRotation.Implementations
+{
+    public class CalculatingNextIndex : ICyclicRotation
+    {
+        public int[] solution(int[] A, int K)
+        {
+            int[] retVal = new int[A.Length];
+
+            for(int i=0; i<A.Length;i++)
+            {
+                int newIndex = (i + K) % A.Length;
+                retVal[newIndex] = A[i];
+            }
+
+            return retVal;
+        }
+    }
+}
